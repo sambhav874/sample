45336ef4d09426cd49b96eecba55d9991d0c8277
sambhav874 <sambhavjain874@gmail.com>
2024-10-23 20:45:07 +0530

    Test 6 to check continous builds

---
+++ my-app/app/test3/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Test1: React.FC = () => {
+  return (
+    <div className="min-h-screen text-black bg-gray-100">
+     This is test 3
+    </div>
+  );
+};
+
+export default Test1;
