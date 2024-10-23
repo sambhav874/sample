11eaf5c5141ff26e3c0a250a8857b242812d838a
sambhav874 <sambhavjain874@gmail.com>
2024-10-23 20:15:30 +0530

    Test 4

---
+++ my-app/app/test/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Test: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-gray-100">
+     Hellooooooooooo
+    </div>
+  );
+};
+
+export default Test;
